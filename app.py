609ecd9341117d9482cd27c6b054f49e902cda07
TalitaNalon <144955008+TalitaNalon@users.noreply.github.com>
2024-10-27 23:17:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,11 @@
 #if __name__ == '__main__':
 #    app.run()
 
-
 from flask import Flask, render_template
 import os
 
-
 app = Flask(__name__)
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+
 # Rota para a página inicial
 @app.route('/')
 def home():
@@ -57,5 +54,5 @@ def testimonial():
 def booking():
     return render_template('booking.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)  # Mantenha o debug como True durante o desenvolvimento
+if __name__ == '__main__':  # Altere de '__DEV__' para '__main__'
+    app.run(debug=True, host='0.0.0.0', port=5000)
