af3ff4193b3296c25d10a8e309b4ad18a96e1198
TalitaNalon <144955008+TalitaNalon@users.noreply.github.com>
2024-10-27 23:05:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,14 @@
 #if __name__ == '__main__':
 #    app.run()
 
+
 from flask import Flask, render_template
+import os
+
 
 app = Flask(__name__)
-
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
 # Rota para a página inicial
 @app.route('/')
 def home():
