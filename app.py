f00226c8f881911308d65bbdae8e9d77c476c4b6
TalitaNalon <144955008+TalitaNalon@users.noreply.github.com>
2024-10-27 22:28:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def booking():
     return render_template('booking.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)  # Mantenha o debug como True durante o desenvolvimento
